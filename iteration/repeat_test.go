97c93be5bb058bd426246f0f673f1783984eb6b2
toshinori sato <tyahhahha@gmail.com>
2020-07-17 16:26:19 +0900

    add iteration package

---
+++ iteration/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a", 10)
+	want := "aaaaaaaaaa"
+
+	if got != want {
+		t.Errorf("expected %q but got %q", want, got)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	got := Repeat("a", 5)
+	fmt.Println(got)
+	// Output: aaaaa
+}
