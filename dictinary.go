b5fecf274f5e59f695e66af7fa89ff4d92e2fdb1
toshinori sato <tyahhahha@gmail.com>
2020-07-18 17:19:08 +0900

    learned map

---
+++ dictinary.go
@@ -0,0 +1,53 @@
+package main
+
+import "errors"
+
+type Dictionary map[string]string
+
+var (
+	ErrNotFound         = errors.New("could not find the word you were looking for")
+	ErrWordExists       = errors.New("cannot add error because it already exists")
+	ErrWordDoseNotExist = errors.New("cannot update because it dose not exist")
+)
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoseNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
